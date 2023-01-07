951da369542556863cdcb21b7e38249954b400dd
Noel Jensen <121671104+sandboxnoel@users.noreply.github.com>
2023-01-07 15:55:01 +0200

    Add files via upload

---
+++ SpliTT/SpliTT/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace SpliTT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.tbRazdelitel = new System.Windows.Forms.TextBox();
+            this.tbPoziciya = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.od = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(421, 171);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tbRazdelitel
+            // 
+            this.tbRazdelitel.Location = new System.Drawing.Point(343, 93);
+            this.tbRazdelitel.Name = "tbRazdelitel";
+            this.tbRazdelitel.Size = new System.Drawing.Size(100, 20);
+            this.tbRazdelitel.TabIndex = 1;
+            this.tbRazdelitel.Text = ":";
+            // 
+            // tbPoziciya
+            // 
+            this.tbPoziciya.Location = new System.Drawing.Point(518, 93);
+            this.tbPoziciya.Name = "tbPoziciya";
+            this.tbPoziciya.Size = new System.Drawing.Size(100, 20);
+            this.tbPoziciya.TabIndex = 2;
+            this.tbPoziciya.Text = "2";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(362, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Razdelitel";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(515, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Poziciya";
+            // 
+            // od
+            // 
+            this.od.FileName = "openFileDialog1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbPoziciya);
+            this.Controls.Add(this.tbRazdelitel);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tbRazdelitel;
+        private System.Windows.Forms.TextBox tbPoziciya;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog od;
+    }
+}
+
---
+++ SpliTT/SpliTT/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpliTT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            od.InitialDirectory = Environment.CurrentDirectory;
+            if (od.ShowDialog() != DialogResult.OK)
+            {
+                return;
+            }
+            List<string> inf = new List<string>(File.ReadAllLines(od.FileName));
+            string oldfn=Path.GetFileName(od.FileName);
+            string path = Path.GetDirectoryName(od.FileName);
+            List<string> outf = new List<string>();
+            string delim = tbRazdelitel.Text;
+            int count = 0;
+            int poz = int.Parse(tbPoziciya.Text);
+            foreach (var item in inf)
+            {
+                count++;
+                try
+                {
+                    outf.Add(item.Split(delim.ToCharArray()[0])[poz - 1]);
+                }
+                catch 
+                {
+                    MessageBox.Show("Строка - "+ item +"\n\r #"+ count.ToString()+"\n\r Error");
+                }
+            }
+            
+            File.WriteAllLines(path+"\\result"+ oldfn, outf);
+            MessageBox.Show("Fin");
+        }
+    }
+}
